b00315e0096ac7e17b6d66564c86fa95ed7410f4
Ivan12390123 <i_van_r@mail.ru>
2025-06-24 15:58:04 +0300

    add logging in the handler and a simple web interface

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/I-Van-Radkov/summer_practice/internal/logic"
@@ -9,40 +10,56 @@ import (
 )
 
 func SolveHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Получены данные")
+
 	var input models.Input
 	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		log.Println("Ошибка при парсинге данных")
 		http.Error(w, "invalid input", http.StatusBadRequest)
 		return
 	}
 
 	yA, err := logic.RungeKutta(input.A, 0, 1, input.E)
 	if err != nil {
+		log.Printf("Ошибка: %v\n", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+
+	log.Printf("Вычислено yA: %v\n", yA)
 
 	zMax, fZMax, err := logic.FindMaximumParallel(input.C, input.D, input.E, yA)
 	if err != nil {
+		log.Printf("Ошибка: %v\n", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
+	log.Printf("Вычислено zMax: %v; fZMax: %v\n", zMax, fZMax)
+
 	areaSimpson, err := logic.IntegrateSimpsonParallel(input.C, zMax, input.E, yA)
 	if err != nil {
+		log.Printf("Ошибка: %v\n", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	areaTrapezoid, err := logic.IntegrateTrapezoidParallel(input.C, zMax, input.E, yA)
 	if err != nil {
+		log.Printf("Ошибка: %v\n", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
+	log.Printf("Вычислено areaSimpson: %v; areaTrapezoid: %v\n", areaSimpson, areaTrapezoid)
+
 	root, err := logic.FindRoot(input.C, input.D, input.E, yA)
 	if err != nil {
+		log.Printf("Ошибка: %v\n", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+
+	log.Printf("Вычислено root: %v\n", root)
 
 	response := models.Output{
 		YA:        yA,
@@ -53,6 +70,5 @@ func SolveHandler(w http.ResponseWriter, r *http.Request) {
 		ZeroPoint: root,
 	}
 
-	w.Header().Set("Content-Type", "application/json")
 	_ = json.NewEncoder(w).Encode(response)
 }
